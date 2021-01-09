f149ef7c28880f7d1bf05a497c1b5e14ea7c5c60
Maycie Morris <maycie.morris@gmail.com>
2021-01-09 18:18:13 -0500

    Minor edits to dark mode

--- src/components/biggerPieces/MainCard.js
+++ src/components/biggerPieces/MainCard.js
@@ -18,18 +18,6 @@ const MainCard = ({ account, ethBalance, tokenBalance }) => {
         setDarkMode(!darkMode);
       };
 
-      const overrideThemeVariables = ({
-        '--light-bg': '#E9B7B9',
-        '--light-bg-dark-shadow': '#ba9294',
-        '--light-bg-light-shadow': '#ffdcde',
-        '--dark-bg': '#292E35',
-        '--dark-bg-dark-shadow': '#21252a',
-        '--dark-bg-light-shadow': '#313740',
-        '--primary': '#8672FB',
-        '--primary-dark': '#4526f9',
-        '--primary-light': '#c7befd'
-      })
-
     const date = new Date().toString()
     
     return (
