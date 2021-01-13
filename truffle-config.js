e1c6c0cdbb8ea44e0a140848638baa9e326ca1ee
Eric Rice <ericmichaelrice@protonmail.com>
2021-01-13 03:03:27 -0500

    fixed config file

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 require('babel-register');
 require('babel-polyfill');
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "pizza absent when future return predict enough drift staff mom lab confirm";
 
 module.exports = {
   networks: {
@@ -8,6 +10,15 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    rinkeby: {
+      provider: function() { 
+       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/0f9fdf2081c34e8680cf758b7903cb59");
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+  }
+    
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
